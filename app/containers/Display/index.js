c6baa9ac838544f0e27e752dde4d5fee8d400bba
fiona <fionamiaojing@gmail.com>
2018-07-31 21:12:12 -0700

    create a display page

---
+++ app/containers/Display/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class Display extends React.PureComponent {
+  render() {
+    return <h1>display page</h1>;
+  }
+}
